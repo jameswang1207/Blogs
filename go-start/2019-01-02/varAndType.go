cfafdce09c4f29e761438a22a111ba5d2d3deb94
jameswang <wangyongxiang@ruijie.com.cn>
2019-01-20 11:29:09 +0800

    add go study note

---
+++ go-start/2019-01-02/varAndType.go
@@ -0,0 +1,17 @@
+// 定义常量组
+package main
+
+import(
+	"fmt"
+	"math"
+)
+
+func main(){
+	var a int 
+	var flag bool
+	var sum float32
+	fmt.Println("james")
+	fmt.Println(a)
+	fmt.Println(flag)
+	fmt.Println(sum)
+}
